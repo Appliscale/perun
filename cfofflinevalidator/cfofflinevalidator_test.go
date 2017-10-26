6f900c7edbdae567447be69a57e75fc29d980fef
Piotr Figwer <piotr.figwer@appliscale.io>
2017-10-26 11:30:45 +0200

    Adding unit tests for offline validator

--- cfofflinevalidator/cfofflinevalidator_test.go
+++ cfofflinevalidator/cfofflinevalidator_test.go
@@ -1,9 +1,54 @@
 package cfofflinevalidator
 
-import "testing"
+import (
+	"testing"
+	"os"
+	"github.com/Appliscale/cftool/cfspecification"
+	"github.com/stretchr/testify/assert"
+)
 
-func TestConverter(t *testing.T) {
-	if false {
-		t.Error("Just an example")
+var specification cfspecification.Specification
+
+func setup() {
+	var err error
+	specification, err = cfspecification.GetSpecificationFromFile("resources/test_specification.json")
+	if err != nil {
+		panic(err)
 	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	retCode := m.Run()
+	os.Exit(retCode)
+}
+
+func TestValidResource(t *testing.T) {
+	resources := make(map[string]Resource)
+	resources["ExampleResource"] = createResourceWithOneProperty("ExampleResourceType", "ExampleProperty", "Property value")
+
+	assert.True(t, validateResources(resources, &specification), "This resource should be valid")
+}
+
+func TestInvalidResourceType(t *testing.T) {
+	resources := make(map[string]Resource)
+	resources["ExampleResource"] = createResourceWithOneProperty("InvalidType", "ExampleProperty", "Property value")
+
+	assert.False(t, validateResources(resources, &specification), "This resource should be valid, it has invalid resource type")
+}
+
+func TestLackOfRequiredPropertyInResource(t *testing.T) {
+	resources := make(map[string]Resource)
+	resources["ExampleResource"] = createResourceWithOneProperty("ExampleResourceType", "SomeProperty", "Property value")
+
+	assert.False(t, validateResources(resources, &specification), "This resource should not be valid, it do not have required property")
+}
+
+func createResourceWithOneProperty(resourceType string, propertyName string, propertyValue string) (Resource) {
+	resource := Resource{}
+	resource.Type = resourceType
+	resource.Properties = make(map[string]interface{})
+	resource.Properties[propertyName] = propertyValue
+
+	return resource
 }
