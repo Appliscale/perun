f3118ee638f27f93cc42f23e69983e9b586f5925
Maksymilian Wojczuk <max.wojczuk@appliscale.io>
2018-03-08 13:03:57 +0100

    Stack Parameters #16
    Added JSON line error reporting - rebased on master

--- helpers/formathelpers.go
+++ helpers/formathelpers.go
@@ -10,6 +10,7 @@ import (
 	"github.com/awslabs/goformation/cloudformation"
 	"github.com/ghodss/yaml"
 	"path"
+	"strconv"
 )
 
 func GetParser(filename string) (func([]byte, template.Template, *logger.Logger) (cloudformation.Template, error), error) {
@@ -27,6 +28,15 @@ func ParseJSON(templateFile []byte, refTemplate template.Template, logger *logge
 
 	err = json.Unmarshal(templateFile, &refTemplate)
 	if err != nil {
+		if syntaxError, isSyntaxError := err.(*json.SyntaxError); isSyntaxError {
+			syntaxOffset := int(syntaxError.Offset)
+			line, character := lineAndCharacter(string(templateFile), syntaxOffset)
+			logger.Error("Syntax error at line " + strconv.Itoa(line) + ", column " + strconv.Itoa(character))
+		} else if typeError, isTypeError := err.(*json.UnmarshalTypeError); isTypeError {
+			typeOffset := int(typeError.Offset)
+			line, character := lineAndCharacter(string(templateFile), typeOffset)
+			logger.Error("Type error at line " + strconv.Itoa(line) + ", column " + strconv.Itoa(character))
+		}
 		return template, err
 	}
 
@@ -63,4 +73,30 @@ func ParseYAML(templateFile []byte, refTemplate template.Template, logger *logge
 
 func PrettyPrintJSON(toPrint interface{}) ([]byte, error) {
 	return json.MarshalIndent(toPrint, "", "    ")
+}
+
+
+func lineAndCharacter(input string, offset int) (line int, character int) {
+	lf := rune(0x0A)
+
+	if offset > len(input) || offset < 0 {
+		return 0, 0
+	}
+
+	line = 1
+
+	for i, b := range input {
+		if b == lf {
+			if i < offset {
+				line++
+				character = 0
+			}
+		} else {
+			character++
+		}
+		if i == offset {
+			break
+		}
+	}
+	return line, character
 }
