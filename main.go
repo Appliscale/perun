7e25a3f8f820f3f10973a94072951454c66352ec
piwowarc <sumetaz@gmail.com>
2018-06-08 11:11:16 +0200

    add mfa command

--- main.go
+++ main.go
@@ -18,15 +18,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Appliscale/perun/cliparser"
 	"github.com/Appliscale/perun/configuration"
 	"github.com/Appliscale/perun/configurator"
 	"github.com/Appliscale/perun/context"
 	"github.com/Appliscale/perun/converter"
+	"github.com/Appliscale/perun/mysession"
 	"github.com/Appliscale/perun/offlinevalidator"
 	"github.com/Appliscale/perun/onlinevalidator"
 	"github.com/Appliscale/perun/stack"
-	"os"
 )
 
 func main() {
@@ -75,6 +77,11 @@ func main() {
 
 	if *context.CliArguments.Mode == cliparser.DestroyStackMode {
 		stack.DestroyStack(&context)
+		os.Exit(0)
+	}
+
+	if *context.CliArguments.Mode == cliparser.MfaMode {
+		mysession.UpdateSessionToken(context.Config.DefaultProfile, context.Config.DefaultRegion, context.Config.DefaultDurationForMFA, &context)
 		os.Exit(0)
 	}
 }
