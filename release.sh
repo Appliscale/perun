7f20ae4cc0e197f321872b23365713489b93e288
sgargula <sylwia.gargula@wp.pl>
2018-07-05 13:12:03 +0200

    with script

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+user="Travis CI"
+email="travis@travis-ci.org"
+perun="perun-linux-amd64"
+github="https://$2@github.com/Appliscale"
+release="https://github.com/Appliscale/perun/releases/download/$1/$perun"
+files="https://raw.githubusercontent.com/Appliscale/perun/master"
+
+git clone https://github.com/Appliscale/rpmbuild.git
+cd rpmbuild/SOURCES
+rm $perun.tar.gz
+wget $release
+wget $files/defaults/main.yaml
+wget $files/LICENSE'
+tar xvzf $perun.tar.gz
+rm $perun.tar.gz
+tar cvzf $perun.tar.gz $perun LICENSE main.yaml
+rm LICENSE main.yaml $perun
+cd ..
+rpmbuild -ba /SPEC/$perun.spec
+git remote
+git config user.email $email
+git config user.name $user
+git add .
+git commit -m "[AUTO] Update RPM by Travis CI"
+git push $github/rpmbuild.git master
+
+cd ~
+git clone https://github.com/Appliscale/perun-dpkg.git
+cd perun-dpkg/usr/local/bin
+rm $perun
+wget $release
+tar xvzf $perun.tar.gz
+rm $perun.tar.gz
+cd ~
+cd perun-dpkg
+dpkg-deb --build perun
+git remote
+git config user.email $email
+git config user.name $user
+git add .
+git commit -m "[AUTO] Update DPKG by Travis CI"
+git push $github/perun-dpkg.git master
