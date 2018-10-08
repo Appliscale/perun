16cede4779e5a289952e2e8c4f9b7d1204ec03e4
sgargula <sylwia.gargula@wp.pl>
2018-10-08 09:15:49 +0200

    tests for creatingfiles

--- checkingrequiredfiles/creatingfiles_test.go
+++ checkingrequiredfiles/creatingfiles_test.go
@@ -8,109 +8,79 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-func TestUseProfileFromConfig(t *testing.T) { //It's ok
+func TestUseProfileFromConfig(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	mockLogger := mocks.NewMockLoggerInt(mockCtrl)
 	defer mockCtrl.Finish()
+	profile := "Test"
 	profilesInConfig := []string{"Test", "Test1"}
 	messages := [3]string{
 		"Available profiles from config:",
 		profilesInConfig[0],
 		profilesInConfig[1],
 	}
-	profile := "Test"
+
 	for _, mes := range messages {
 		mockLogger.EXPECT().Always(mes).Times(1)
 	}
 	mockLogger.EXPECT().GetInput("Which profile should perun use as a default?", &profile).Return(nil).Times(1)
+
 	useProfileFromConfig(profilesInConfig, profile, mockLogger)
 }
 
-func TestAddNewProfileFromCredentialsToConfig(t *testing.T) { // "I found profile perun..." want "Region, Output" x3
+func TestAddNewProfileFromCredentialsToConfig(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	mockLogger := mocks.NewMockLoggerInt(mockCtrl)
 	defer mockCtrl.Finish()
-
 	profile := "perun"
-	region := "us-east-1"
-	output := "json"
 	homePath := "./test_resources"
 	templatePath := "../stack/test_resources/test_template.yaml"
 	ctx := mockContext.SetupContext(t, []string{"cmd", "create-stack", "teststack", templatePath})
 
-	ans := "Y"
-
-	data := [3]string{
-		ans,
-		region,
-		output,
-	}
-	messages := [3]string{
-		"I found profile " + profile + " in credentials, but not in config. \nCreate new profile in config? Y/N",
-		"Region",
-		"Output",
-	}
-	for i, mes := range messages {
-		mockLogger.EXPECT().GetInput(mes, &data[i]).Return(nil).Times(1)
-	}
+	mockLogger.EXPECT().GetInput("I found profile "+profile+" in credentials, but not in config. \nCreate new profile in config? Y/N", gomock.Any()).Return(nil).Times(1)
 
 	addNewProfileFromCredentialsToConfig("default", homePath, ctx, mockLogger)
 }
 
-func TestAddProfileToCredentials(t *testing.T) { //missing calls
+func TestAddProfileToCredentials(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	mockLogger := mocks.NewMockLoggerInt(mockCtrl)
 	defer mockCtrl.Finish()
-	profile := "test1"
+	profile := "perun"
 	homePath := "./test_resources"
 	templatePath := "../stack/test_resources/test_template.yaml"
-	awsAccessKeyID := "TESTTEST"
-	awsSecretAccessKey := "testtest"
-	mfaSerial := "arn:aws:iam:"
-
-	messages := [3]string{
-		"awsAccessKeyID",
-		"awsSecretAccessKey",
-		"mfaSerial",
-	}
-	data := [3]string{
-		awsAccessKeyID,
-		awsSecretAccessKey,
-		mfaSerial,
-	}
 	ctx := mockContext.SetupContext(t, []string{"cmd", "create-stack", "teststack", templatePath})
-	mockLogger.EXPECT().Always("You haven't got .aws/credentials file for profile " + profile).Times(1)
 
-	for i, mes := range messages {
-		mockLogger.EXPECT().GetInput(mes, &data[i]).Return(nil).Times(1)
-	}
+	mockLogger.EXPECT().Always("Profile " + profile + " has already credentials").Times(1)
 
 	addProfileToCredentials(profile, homePath, ctx, mockLogger)
 }
 
-func TestConfigIsPresent(t *testing.T) { // Available profiles from config, default profiles doesn't exist ( but it does)
+func TestConfigIsPresent(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	mockLogger := mocks.NewMockLoggerInt(mockCtrl)
 	defer mockCtrl.Finish()
-	profile := "prof"
+	profile := "default"
 	homePath := "./test_resources"
 	templatePath := "../stack/test_resources/test_template.yaml"
-	answer := "Y"
 	ctx := mockContext.SetupContext(t, []string{"cmd", "create-stack", "teststack", templatePath})
-	mockLogger.EXPECT().GetInput("Default profile exists, do you want to use it *Y* or create your own *N*?", &answer).Return(nil).Times(1)
+
+	mockLogger.EXPECT().GetInput("Default profile exists, do you want to use it *Y* or create your own *N*?", gomock.Any()).Return(nil).Times(1)
+
 	configIsPresent(profile, homePath, ctx, mockLogger)
 }
 
-func TestCreateCredentials(t *testing.T) { //unexpected call, arguments doesn't match
+func TestCreateCredentials(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	mockLogger := mocks.NewMockLoggerInt(mockCtrl)
 	defer mockCtrl.Finish()
 	profile := "test1"
 	homePath := "./test_resources"
 	templatePath := "../stack/test_resources/test_template.yaml"
-	answer := "Y"
 	ctx := mockContext.SetupContext(t, []string{"cmd", "create-stack", "teststack", templatePath})
-	mockLogger.EXPECT().GetInput("I found profile "+profile+" in .aws/config without credentials, add? Y/N", &answer)
+
+	mockLogger.EXPECT().GetInput("I found profile "+profile+" in .aws/config without credentials, add? Y/N", gomock.Any())
+
 	createCredentials(profile, homePath, ctx, mockLogger)
 
 }
