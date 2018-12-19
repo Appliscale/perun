1463f3da03a394da6dbef776053f5e9062fe3c92
sgargula <sylwia.gargula@wp.pl>
2018-12-19 09:07:41 +0100

    Fn::ImportValue

--- helpers/formathelpers_test.go
+++ helpers/formathelpers_test.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"github.com/awslabs/goformation/cloudformation"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
@@ -40,4 +41,32 @@ func TestLineAndCharacter(t *testing.T) {
 	line, character = lineAndCharacter("0123456789\nasd", 11)
 	assert.Equal(t, 2, line)
 	assert.Equal(t, 1, character)
+}
+
+func TestFindFnImportValue(t *testing.T) {
+	var templateFile = make([]byte, 0)
+	tempYAML := cloudformation.Template{}
+	err := findFnImportValue(templateFile, &tempYAML)
+	assert.Nilf(t, err, "Error should be nil")
+}
+
+func TestReplaceImportValue(t *testing.T) {
+	path := []string{"Resorce", "Property", "Name", "Value"}
+	cfTemplate := cloudformation.Template{}
+
+	err := replaceImportValue(path, &cfTemplate)
+	assert.NotNilf(t, err, "Error should not be nil")
+
+}
+
+func TestAddToPathAndReplace(t *testing.T) {
+	path := []string{}
+	name := "name"
+	value := "value"
+	tempYAML := cloudformation.Template{}
+	startPath := []string{}
+
+	err := addToPathAndReplace(path, name, value, &tempYAML, startPath)
+	assert.Nilf(t, err, "Error should be nil")
+
 }
